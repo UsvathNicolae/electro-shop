7b6517f545a72e30bb7edc351b777f4bc78e6bf8
Mita Daniel <daniel@Mitas-MacBook-Pro.local>
2022-04-02 14:10:17 +0300

    Connected sequalize to mysql running in docker container

---
+++ server/src/configs/sequelize/connection.ts
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const sequelize = new Sequelize(
+    process.env.DATABASE_NAME,
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,
+    { host: process.env.HOST, dialect: process.env.DATABASE_DIALECT },
+);
+
+module.exports = sequelize;
