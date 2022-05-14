3c99f6c355633111ca767c61e8b18f13c0c32ca3
Nicu <nicu_usvat@yahoo.com>
2022-05-14 16:32:32 +0300

    Added login buton adn snackbar

--- web_client/src/pages/LoginPage.tsx
+++ web_client/src/pages/LoginPage.tsx
@@ -1,7 +1,6 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {
     Typography,
-    Grid,
     Table,
     TableBody,
     TableCell,
@@ -14,12 +13,72 @@ import {
 } from '@mui/material';
 import { makeStyles } from '@mui/styles'
 import Navbar from "../components/NavBar";
+import LoginType from "../types/loginType";
+import { useAppDispatch } from "../redux/hooks"
+import { setOpen } from "../redux/reducers/snackbarReducer"
 
 
 const useStyles = makeStyles({})
 
 const LoginPage = () => {
     const styles = useStyles()
+
+    const [loginData, setLoginData] = useState <LoginType>({
+        email:"",
+        password:"",
+    })
+
+    const [disable, setDisable] = useState<boolean>(false)
+
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        const bool = !loginData.email || !loginData.password
+        setDisable(bool)
+    },[loginData])
+
+    const handleOnChange = (e:any) => {
+        e.preventDefault()
+        setLoginData({
+            ...loginData,[e.target.name]:e.target.value
+        })
+    }
+
+    const handleOnSubmit = async () =>{
+
+        await fetch("http://localhost:8080/user/login", {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(loginData),
+        }).then((data) => data.json()).then((data) => {
+            if(data.token){
+                localStorage.setItem('token', data.token)
+            }
+            if(data.message){
+                dispatch(setOpen({
+                    success: true,
+                    message: "Authentication successfully"
+                }))
+            }else{
+                dispatch(setOpen({
+                    success: false,
+                    message: "Authentication failed"
+                }))
+            }
+
+        }).catch((error) => {
+            console.error(error)
+        })
+
+        setLoginData({
+            email:"",
+            password:"",
+        })
+    }
+
+    const submit = () => {
+
+    }
 
     return (
         <div>
@@ -38,11 +97,14 @@ const LoginPage = () => {
                             </TableCell>
                             <TableCell>
                                 <TextField
+                                    name = "email"
                                     type = "text"
                                     id = "standard-required"
                                     label = "Email"
                                     placeholder = "email@address.com"
                                     variant = "standard"
+                                    onChange = {(e:any) => handleOnChange(e)}
+                                    value = { loginData.email }
                                 />
 
                             </TableCell>
@@ -55,12 +117,23 @@ const LoginPage = () => {
                             </TableCell>
                             <TableCell>
                                 <TextField
+                                    name = "password"
                                     id="standard-password-input"
                                     label="Password"
                                     type="password"
                                     autoComplete="current-password"
                                     variant="standard"
+                                    onChange = {(e:any) => handleOnChange(e)}
+                                    value = { loginData.password }
                                 />
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell></TableCell>
+                            <TableCell align = "center">
+                                <Button color="primary" variant="contained" onClick = { handleOnSubmit } disabled={ disable }>
+                                    Login
+                                </Button>
                             </TableCell>
                         </TableRow>
                     </TableBody>
